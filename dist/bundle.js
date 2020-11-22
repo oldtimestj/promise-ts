788cbe567d2fb951d9a5b908ce8cbbf669048c67
oldtimestj <85237486@qq.com>
2020-11-22 23:21:33 +0800

    before方法实现

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,49 @@
 'use strict';
 
-var str = 'hello';
-console.log(str);
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+
+function __spreadArrays() {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+}
+
+// promise都是基于回调模式的
+Function.prototype.before = function (fn) {
+    var _this = this;
+    // this 箭头函数里没有this会向上查找
+    return function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        fn(); //先调用before方法
+        _this.apply(void 0, args); //在调用原有的core方法 把数组展开
+    };
+};
+function core() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    console.log.apply(console, __spreadArrays(['core...'], args));
+}
+var fn = core.before(function () {
+    console.log('before core...');
+});
+fn(1, 2, 3);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,38 @@
-let str:string = 'hello';
-console.log(str);
+// promise都是基于回调模式的
+
+// 高阶函数
+// 1、如果你的函数的参数是一个函数
+// 2、如果一个函数返回一个函数
+
+
+// 基于源代码扩展
+
+type Callback = () => void;
+type ReturnFn = (...args:any[]) => void;
+declare global {
+  interface Function {  //接口的合并
+    before(fn:Callback):ReturnFn;
+  } 
+}
+
+
+Function.prototype.before = function (fn) {
+  // this 箭头函数里没有this会向上查找
+  return (...args) => {  //此处是把所有参数转变为一个数组
+    fn();  //先调用before方法
+    this(...args); //在调用原有的core方法 把数组展开
+  }
+}
+
+function core(...args) {
+  console.log('core...', ...args);
+}
+
+let fn = core.before(() => {
+  console.log('before core...');
+
+})
+
+fn(1, 2, 3);
+
+export {}
